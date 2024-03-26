2c71445c3027144a5569392ee648fd7c07ee0ce7
STRYKER316 <ansil.mishra316@gmail.com>
2024-03-26 14:44:59 +0530

    Set Meta information for Category model

--- category/models.py
+++ category/models.py
@@ -8,5 +8,9 @@ class Category(models.Model):
     description = models.TextField(blank=True)
     category_image = models.ImageField(upload_to='photos/categories', blank=True)
 
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
     def __str__(self):
         return self.category_name
