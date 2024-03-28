5a007832b9cd28bc1921c98dabe3942c59a3c161
STRYKER316 <ansil.mishra316@gmail.com>
2024-03-28 21:17:34 +0530

    Create a model form for user registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 
+from .forms import RegistrationForm
+
 # Create your views here.
 
 def register(reuqest):
-    return render(reuqest, 'accounts/register.html')
+    form = RegistrationForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(reuqest, 'accounts/register.html', context)
 
 
 def login(reuqest):
