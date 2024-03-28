41b35968f0e80e1132e4f4e1092968be89e3b0a1
STRYKER316 <ansil.mishra316@gmail.com>
2024-03-28 20:52:12 +0530

    Create HTML templates for register and login pages

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def register(reuqest):
+    return render(reuqest, 'accounts/register.html')
+
+
+def login(reuqest):
+    return render(reuqest, 'accounts/login.html')
+
+
+def logout(reuqest):
+    return render()
