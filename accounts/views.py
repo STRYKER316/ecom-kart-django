eb5e8a769f8532fa72e9bea337927923ea08358d
STRYKER316 <ansil.mishra316@gmail.com>
2024-03-28 22:11:18 +0530

    Implement functionality to handle user registration form submission

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from .models import Account
 
 from .forms import RegistrationForm
 
 # Create your views here.
 
 def register(reuqest):
-    form = RegistrationForm()
+    if reuqest.method == 'POST':
+        form = RegistrationForm(reuqest.POST)
+
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = email.split('@')[0]
+
+            user = Account.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                username=username,
+                email=email,
+                password=password
+            )
+            user.phone_number = phone_number
+            user.save()
+
+    # GET Request
+    else:
+        form = RegistrationForm()
 
     context = {
-        'form': form,
+        'form': form
     }
 
     return render(reuqest, 'accounts/register.html', context)
