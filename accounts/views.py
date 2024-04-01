df8fffc90fe7aa1cf67f934a79ee49b67c4b1e78
STRYKER316 <ansil.mishra316@gmail.com>
2024-04-01 15:51:49 +0530

    Set up SMTP configs to send account activation link after successful user registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import redirect, render
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+from django.http import HttpResponse
 
 from .models import Account
 
@@ -31,6 +38,20 @@ def register(reuqest):
             )
             user.phone_number = phone_number
             user.save()
+
+            # User Activation
+            current_site = get_current_site(reuqest)
+            mail_subject = 'Please activate your account'
+            message = render_to_string('accounts/account_verification_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user)
+            })
+
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
 
             messages.success(reuqest, 'Registration Successful')
 
@@ -76,3 +97,21 @@ def logout(reuqest):
 @login_required(login_url='login')
 def dashboard(reuqest):
     return render(reuqest, 'accounts/dashboard.html')
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+
+    except (TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user != None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Your account is now activated!')
+        return redirect('login')
+
+    messages.error(request, 'Invalid activation link')
+    return redirect('register')
