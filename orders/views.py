13c01f7aabbec3e39db65f43ea960a4127beffda
STRYKER316 <ansil.mishra316@gmail.com>
2024-04-03 20:05:33 +0530

    Add place order functionality with billing details

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+import datetime
+
+from django.shortcuts import render, redirect
+
+from .models import Order
+from .forms import OrderForm
+from cart.models import CartItem
+
 
 # Create your views here.
+def place_order(request, total=0, quantity=0):
+    current_user = request.user
+
+    # if cart item count = 0, then redirect to store
+    cart_items = CartItem.objects.filter(user=current_user)
+    cart_count = cart_items.count()
+    if cart_count <= 0:
+        return redirect('store')
+
+    grand_total = 0
+    tax = 0
+    for cart_item in cart_items:
+        total += (cart_item.product.price * cart_item.quantity)
+        quantity += cart_item.quantity
+    tax = (2 * total) / 100
+    grand_total = total + tax
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            # store all the billing info
+            data = Order()
+            data.user = current_user
+            data.first_name = form.cleaned_data['first_name']
+            data.last_name = form.cleaned_data['last_name']
+            data.phone = form.cleaned_data['phone']
+            data.email = form.cleaned_data['email']
+            data.address_line_1 = form.cleaned_data['address_line_1']
+            data.address_line_2 = form.cleaned_data['address_line_2']
+            data.country = form.cleaned_data['country']
+            data.state = form.cleaned_data['state']
+            data.city = form.cleaned_data['city']
+            data.order_note = form.cleaned_data['order_note']
+            data.order_total = grand_total
+            data.tax = tax
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+
+            # generate order number
+            year = int(datetime.date.today().strftime('%Y'))
+            month = int(datetime.date.today().strftime('%m'))
+            date = int(datetime.date.today().strftime('%d'))
+            d = datetime.date(year, month, date)
+            current_date = d.strftime('%Y%m%d')
+            order_number = current_date + str(data.id)
+            data.order_number = order_number
+            data.save()
+
+            return redirect('checkout')
+
+        return redirect('checkout')
