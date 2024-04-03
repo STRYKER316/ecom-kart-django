db489f06c53f1537719c7f31a061f3f518305311
STRYKER316 <ansil.mishra316@gmail.com>
2024-04-03 20:46:40 +0530

    Set up Order Review page and functionality

--- orders/models.py
+++ orders/models.py
@@ -48,6 +48,12 @@ class Order(models.Model):
     def __str__(self):
         return self.first_name
 
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def full_address(self):
+        return f'{self.address_line_1} {self.address_line_2}'
+
 
 class OrderProduct(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
--- orders/views.py
+++ orders/views.py
@@ -7,7 +7,10 @@ from .forms import OrderForm
 from cart.models import CartItem
 
 
-# Create your views here.
+def payments(request):
+    return render(request, 'orders/payments.html')
+
+
 def place_order(request, total=0, quantity=0):
     current_user = request.user
 
@@ -56,6 +59,15 @@ def place_order(request, total=0, quantity=0):
             data.order_number = order_number
             data.save()
 
-            return redirect('checkout')
+            order = Order.objects.get(user=current_user, is_ordered=False, order_number=order_number)
+            context = {
+                'order': order,
+                'cart_items': cart_items,
+                'total': total,
+                'tax': tax,
+                'grand_total': grand_total,
+            }
+
+            return render(request, 'orders/payments.html', context)
 
         return redirect('checkout')
