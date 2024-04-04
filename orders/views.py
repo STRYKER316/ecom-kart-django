22aab16dac4cd578485b81b30acd6ca11ba8d68e
STRYKER316 <ansil.mishra316@gmail.com>
2024-04-04 23:56:52 +0530

    Add functionality for sending order received email to users

--- orders/views.py
+++ orders/views.py
@@ -3,6 +3,8 @@ import json
 import razorpay
 
 from django.shortcuts import render, redirect
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 
 from .models import Order, Payment, OrderProduct
 from cart.models import CartItem
@@ -62,7 +64,15 @@ def payments(request):
     CartItem.objects.filter(user=request.user).delete()
 
     # Send order emails
+    mail_subject = 'Thank you for your order!'
+    message = render_to_string('orders/order_received_email.html', {
+        'user': request.user,
+        'order': order,
+    })
 
+    to_email = request.user.email
+    send_email = EmailMessage(mail_subject, message, to=[to_email])
+    send_email.send()
 
     # Send order number and transaction id back to sendData method via JsonResponse
 
