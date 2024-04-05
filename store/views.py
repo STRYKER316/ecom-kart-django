6a121648c493e6a33e75018b678536502bc388f1
STRYKER316 <ansil.mishra316@gmail.com>
2024-04-05 11:52:09 +0530

    Implement functionality for processing and storing user reviews

--- store/views.py
+++ store/views.py
@@ -1,13 +1,15 @@
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Q
+from django.contrib import messages
 
 from cart.models import CartItem
 from cart.views import _get_cart_id
 from category.models import Category
+from .models import Product, ReviewRating
+from .forms import ReviewForm
 
-from .models import Product
 
 # Create your views here.
 def store(request, category_slug=None):
@@ -70,3 +72,33 @@ def search(request):
     }
 
     return render(request, 'store/store.html', context)
+
+
+def submit_review(request, product_id):
+    url = request.META.get('HTTP_REFERER')
+
+    # POST request
+    if request.method == 'POST':
+        try:
+            reviews = ReviewRating.objects.get(user__id=request.user.id, product__id=product_id)
+            form = ReviewForm(request.POST, instance=reviews)
+            form.save()
+            messages.success(request, 'Thank you! Your review has been updated.')
+            return redirect(url)
+
+        except ReviewRating.DoesNotExist:
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                data = ReviewRating()
+                data.subject = form.cleaned_data['subject']
+                data.review = form.cleaned_data['review']
+                data.rating = form.cleaned_data['rating']
+                data.ip = request.META.get('REMOTE_ADDR')
+                data.product_id = product_id
+                data.user_id = request.user.id
+                data.save()
+                messages.success(request, 'Thank you! Your review has been submitted.')
+                return redirect(url)
+
+    # GET request
+    return redirect(url)
