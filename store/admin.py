93962a7176381aa18efc546ea3ed1f764666a162
STRYKER316 <ansil.mishra316@gmail.com>
2024-04-06 14:28:07 +0530

    Create Product Gallery model

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Product, Variation, ReviewRating
+from .models import Product, Variation, ReviewRating, ProductGallery
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -18,3 +18,4 @@ class VariationAdmin(admin.ModelAdmin):
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Variation, VariationAdmin)
 admin.site.register(ReviewRating)
+admin.site.register(ProductGallery)
