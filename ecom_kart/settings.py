171af0b22286957cc3ae0fe6775eed0086bb1dde
STRYKER316 <ansil.mishra316@gmail.com>
2024-04-07 14:47:10 +0530

    Update ALLOWED_HOSTS in settings.py

--- ecom_kart/settings.py
+++ ecom_kart/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY', default='jkhksdgksdsadjasfdhasfdaskdatejasjdga
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ecom-kart-env.eba-cqky5dsz.ap-south-1.elasticbeanstalk.com', '172.31.10.140', 'localhost']
 
 
 # Application definition
